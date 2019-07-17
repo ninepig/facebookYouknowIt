58e880d1eedf4fd41cf57f0fc6c56d1c9e972555
ninepig <ywj1988>
2019-07-17 12:38:23 -0400

    7.17before that

--- src/fbMianjing/NonLeetCodeSeaLineProblem.java
+++ src/fbMianjing/NonLeetCodeSeaLineProblem.java
@@ -1,5 +1,9 @@
 package fbMianjing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created by yangw on 2019/7/17.
  */
@@ -10,23 +14,25 @@ public class NonLeetCodeSeaLineProblem {
  * 于是面试官问能否一次扫描解决。我说在想有没有什么数据结构可以做，面试官说不用，解不出来，还请大神指教了。
  *
  * 用stack就行。从左往右扫。栈顶遇到高的就pop。遇到低的直接放入。最后栈里元素就是答案
- * /
-         Stack<Integer> stack = new Stack<>();
-            int i = 0;
-         while (i < a.length) {
-         if (stack.isEmpty() || a[stack.peek()] > a) {
-         stack.push(i++);
-         } else {
-         while (!stack.isEmpty() && a[stack.peek()] <= a) {
-         stack.pop();
-         }
-         }
-         }
+ */
+    public List<Integer> seeShareSide(int[] a) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        while (i < a.length) {
+            if (stack.isEmpty() || a[stack.peek()] > a[i]) {
+                stack.push(i++);
+            } else {
+                while (!stack.isEmpty() && a[stack.peek()] <= a[i]) {
+                    stack.pop();
+                }
+            }
+        }
 
-         List<Integer> result = new ArrayList<>();
-         while (!stack.isEmpty()) {
-         result.add(stack.pop());
-         }
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
 
-         return result;
+        return result;
+    }
 }
